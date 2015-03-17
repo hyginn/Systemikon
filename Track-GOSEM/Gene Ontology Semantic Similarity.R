f42618e33af0087da34c21bce3272e6c489eee30
wdevega <Wilfred@dhcphost-sw135.utsc.utoronto.ca>
2015-03-17 12:09:10 -0400

    Corrected some syntax in R script

--- Track-GOSEM/Gene Ontology Semantic Similarity.R
+++ Track-GOSEM/Gene Ontology Semantic Similarity.R
@@ -10,7 +10,7 @@ genecombo <- t(combn(LoGtable[,"Entrez.ID"], m = 2)) #obtain every possible comb
 colnames(genecombo)[1:2] <- c("Gene 1", "Gene 2") #Rename the first two columns for clarity
 
 CCscore <- apply(genecombo, 1, function(x){ #Calculate CC GO Semantic Similarity Scores over each row (1) of the matrix
-  CCSemSim <- geneSim(x[1], x[2], ont = 'CC', organism = 'human', measure = 'Rel', combine = 'BMA') #should return a vector of 3 elements
+  CCSemSim <- geneSim(x[1], x[2], ont = "CC", organism = "human", measure = "Rel", combine = "BMA") #should return a vector of 3 elements
   if(length(CCSemSim) < 3){ #if a vector of a length < 3 is returned (ie. calculation fails due to lack of GO annotations)
     CC <- 0 #assign a CC score of 0
   }
@@ -25,7 +25,7 @@ CCscore <- apply(genecombo, 1, function(x){ #Calculate CC GO Semantic Similarity
   }})
 
 BPscore <- apply(genecombo, 1, function(x){ #Calculate BP GO Semantic Similarity Scores over each row (1) of the matrix
-  BPSemSim <- geneSim(x[1], x[2], ont = 'BP', organism = 'human', measure = 'Rel', combine = 'BMA')
+  BPSemSim <- geneSim(x[1], x[2], ont = "BP", organism = "human", measure = "Rel", combine = "BMA")
   if(length(BPSemSim) < 3){ #if a vector of a length < 3 is returned (ie. calculation fails due to lack of GO annotations)
     BP <- 0 #assign a BP score of 0
   }
@@ -42,5 +42,5 @@ BPscore <- apply(genecombo, 1, function(x){ #Calculate BP GO Semantic Similarity
 RawScores <- cbind(CCscore, BPscore) #combine raw scores into table
 GOSEMScore <- rowMeans(RawScores) #calculate average of raw GO Semantic Similarity Score
 TOTALGOSEM <- cbind(genecombo, GOSEMScore) #compile table with all gene pairs and GOSEMScores
-passthresh <- which(TOTALGOSEM[,'GOSEMScore'] > 0.2) #determines indices of the table (gene pairs) that pass the 0.2 threshold
-GOSemSim <- TOTALGOSEM[passthresh,] #produce a table with gene pairs that GO Semantic Similarity threshold
+passthresh <- which(TOTALGOSEM[,"GOSEMScore"] > 0.2) #determines indices of the table (gene pairs) that pass the 0.2 threshold
+GOSemSim <- TOTALGOSEM[passthresh,] #produce a table with gene pairs that pass GO Semantic Similarity threshold
